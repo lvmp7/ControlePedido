517b79cc60f8019bc2d1d6743fcd6bb3b18c6da7
Luiz Vinicius <luiz.vmpereira@gmail.com>
2019-03-19 21:09:23 -0300

    Editando PedidoFileDataSource

--- src/br/com/vv/controlePedido/PedidoFileDataSource.java
+++ src/br/com/vv/controlePedido/PedidoFileDataSource.java
@@ -1,26 +1,37 @@
 package br.com.vv.controlePedido;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import br.com.vv.controlePedido.interfaces.PedidoDataSource;
+import br.com.vv.controlePedido.model.MontarConsole;
 import br.com.vv.controlePedido.model.Pedido;
 
 public class PedidoFileDataSource implements PedidoDataSource {
-
+	private List<Pedido> pedidos;
 	private List<File> files;
+	
+	public PedidoFileDataSource() {
+		// TODO Auto-generated constructor stub
+		files = (List<File>) FileUtils.listFiles(new File("pedidos"), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+		for (File file : files) {
+			convert(file);
+			System.out.println(file.getAbsolutePath());
+		}
+	}
 	
 	/**
 	 * lista de pedidos
 	 */
-	public void listar(){
-		// TODO Auto-generated method stub
-		files = (List<File>) FileUtils.listFiles(new File("pedidos"), TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
-		for (File file : files) {
-			System.out.println(file.getAbsolutePath());
+	public void listar(){		
+		for (Pedido pedido : pedidos) {
+			MontarConsole.showLista(pedido);
 		}
 	}
 	/**
@@ -30,6 +41,13 @@ public class PedidoFileDataSource implements PedidoDataSource {
 	public void incluir(Pedido pedido) {
 		// TODO Auto-generated method stub
 		
+		try {
+			ObjectOutputStream o = new ObjectOutputStream(FileUtils.openOutputStream(new File("pedidos\\"+pedido.getCodigo())));
+			o.writeObject(pedido);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	/**
 	 * Altera um Pedido
@@ -58,6 +76,20 @@ public class PedidoFileDataSource implements PedidoDataSource {
 	 */
 	public Pedido find(int codigo) {
 		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private Pedido convert(File file) {
+		Pedido p;
+		try {
+			ObjectInputStream o = new ObjectInputStream(FileUtils.openInputStream(file));
+			p = (Pedido) o.readObject();
+			return p;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
